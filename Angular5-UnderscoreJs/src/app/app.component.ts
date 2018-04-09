95ed1d47f31961adff47013a97864d889893a67a
Anthony Giretti <anthony.giretti@gmail.com>
2018-04-08 21:05:57 -0400

    add amples

--- Angular5-UnderscoreJs/src/app/app.component.ts
+++ Angular5-UnderscoreJs/src/app/app.component.ts
@@ -15,10 +15,10 @@ export class AppComponent implements OnInit {
   public ngOnInit() {
 
     // Collections
-    this._helper.Collections.each(["Apple", "Banana", "Grapefruit"], console.log);
-    this._helper.Collections.each({one: 1, two: 2, three: 3}, console.log);
+    this._helper.Collections.each(["Apple", "Banana", "Grapefruit"], (value, key) => {console.log("Value :" + value + " - "+ "Key :" + key)});
+    this._helper.Collections.each({one: 1, two: 2, three: 3}, (value, key) => {console.log("Value :" + value + " - "+ "Key :" + key)});
     this._helper.Collections.map([1, 2, 3], function(num){ return num * 3; });
-    this._helper.Collections.map({one: 1, two: 2, three: 3}, (num) =>{ return num * 3; });
+    this._helper.Collections.map({one: 1, two: 2, three: 3}, (num) =>{ return num * 2; });
 
 
     // Arrays 
