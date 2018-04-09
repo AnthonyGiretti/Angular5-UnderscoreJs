e848958d19194cf62a23b96cc0109ea64c0a12eb
Anthony Giretti <anthony.giretti@gmail.com>
2018-04-08 20:51:08 -0400

    Add examples is component.ts

--- Angular5-UnderscoreJs/src/app/app.component.ts
+++ Angular5-UnderscoreJs/src/app/app.component.ts
@@ -9,16 +9,20 @@ import { HelpersService } from './services/helpers.service';
 })
 export class AppComponent implements OnInit {
 
-  private _fruits = ["Apple", "Banana", "Grapefruit"];
 
   constructor(private _helper: HelpersService) { }
 
   public ngOnInit() {
 
-    this._helper.Collections.each(this._fruits, console.log);
+    // Collections
+    this._helper.Collections.each(["Apple", "Banana", "Grapefruit"], console.log);
+    this._helper.Collections.map([1, 2, 3], function(num){ return num * 3; });
+    this._helper.Collections.map({one: 1, two: 2, three: 3}, (num) =>{ return num * 3; });
 
-    console.log(this._helper.Arrays.first(this._fruits));
-    console.log(this._helper.Arrays.first(this._fruits, 2));
+
+    // Arrays 
+    console.log(this._helper.Arrays.first( ["Apple", "Banana", "Grapefruit"]));
+    console.log(this._helper.Arrays.first( ["Apple", "Banana", "Grapefruit"], 2));
 
   }
 
