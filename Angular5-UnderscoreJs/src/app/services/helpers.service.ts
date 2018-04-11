1e368db679ffd03e51c4950fb6c7f3209e08f24e
Anthony Giretti <anthony.giretti@gmail.com>
2018-04-10 20:40:00 -0400

    remove link to each service from Angular, just helper service is binded

--- Angular5-UnderscoreJs/src/app/services/helpers.service.ts
+++ Angular5-UnderscoreJs/src/app/services/helpers.service.ts
@@ -7,9 +7,15 @@ import { ObjectsService } from './objects';
 @Injectable()
 export class HelpersService {
 
-  constructor(public readonly Collections: CollectionsService,
-              public readonly Arrays: ArraysService,
-              public readonly Functions: FunctionsService,
-              public readonly Objects: ObjectsService) { 
+  public readonly Collections: CollectionsService;
+  public readonly Arrays: ArraysService;
+  public readonly Functions: FunctionsService;
+  public readonly Objects: ObjectsService;
+
+  constructor() { 
+    this.Collections = new CollectionsService();
+    this.Arrays = new ArraysService();
+    this.Functions = new FunctionsService();
+    this.Objects = new ObjectsService();   
   }
 }
