acf93f3e4dd8ff6bf240b28a1bad3269259d0234
Anthony Giretti <anthony.giretti@gmail.com>
2018-04-14 20:20:41 -0400

    add min function declaration

--- Angular5-UnderscoreJs/src/app/services/collections.ts
+++ Angular5-UnderscoreJs/src/app/services/collections.ts
@@ -177,4 +177,15 @@ export class CollectionsService {
     return _.max(list, delegate);
   }
 
+  /**
+   *Returns the minimum value in list. If an iteratee function is provided, it will be used on each value to generate the criterion by which the value is ranked. 
+   Infinity is returned if list is empty, so an isEmpty guard may be required. 
+   Non-numerical values in list will be ignored.
+   * @param list 
+   * @param delegate 
+   */
+  public min(list: Array<any>, delegate: Function): any {
+    return _.min(list, delegate);
+  }
+
 }
