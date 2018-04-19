d636bbf695b861018c4cc23868ffe88abe74038e
Anthony Giretti <anthony.giretti@gmail.com>
2018-04-18 20:13:47 -0400

    add initial array method declaration

--- Angular5-UnderscoreJs/src/app/services/arrays.ts
+++ Angular5-UnderscoreJs/src/app/services/arrays.ts
@@ -11,12 +11,26 @@ export class ArraysService {
    * @param array 
    * @param n 
    */
-  public first(array:any[], n?: number): any | any[] {
+  public first(array:Array<any>, n?: number): any | Array<any> {
     
     if (null == n)
         return _.first(array);
     
     return _.first(array, n);
+  }
+
+  /**
+   * Returns everything but the last entry of the array. Especially useful on the arguments object. 
+   * Pass n to exclude the last n elements from the result.
+   * @param array 
+   * @param n 
+   */
+  public initial(array:Array<any>, n?: number): Array<any> {
+    
+    if (null == n)
+        return _.initial(array);
+    
+    return _.initial(array, n);
   }
 
 }
