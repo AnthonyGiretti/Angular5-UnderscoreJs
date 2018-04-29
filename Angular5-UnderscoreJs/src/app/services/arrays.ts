4c4094456afc207ba2b9e033bcc634aac7a66637
Anthony Giretti <anthony.giretti@gmail.com>
2018-04-28 22:02:43 -0400

    add union method declaration

--- Angular5-UnderscoreJs/src/app/services/arrays.ts
+++ Angular5-UnderscoreJs/src/app/services/arrays.ts
@@ -82,4 +82,12 @@ export class ArraysService {
     return _.without(array, ...valuesToRemove);
   }
 
+  /**
+   * Computes the union of the passed-in arrays: the list of unique items, in order, that are present in one or more of the arrays.
+   * @param array 
+   */
+  public union<T>(...arraysToMerge: Array<T>): Array<T> {
+    return _.union(...arraysToMerge);
+  }
+
 }
