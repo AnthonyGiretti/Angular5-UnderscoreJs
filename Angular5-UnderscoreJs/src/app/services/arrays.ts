5b708c5e1803b62ca07db16ad36608645a721032
Anthony Giretti <anthony.giretti@gmail.com>
2018-04-29 19:38:24 -0400

    add indexof method declaration

--- Angular5-UnderscoreJs/src/app/services/arrays.ts
+++ Angular5-UnderscoreJs/src/app/services/arrays.ts
@@ -162,4 +162,16 @@ export class ArraysService {
   public objectFromArrays(arrays: Array<any>): object {
     return _.object(arrays);
   }
+
+  /** 
+   * Returns the index at which value can be found in the array, or -1 if value is not present in the array. 
+   * If you're working with a large array, 
+   * and you know that the array is already sorted, pass true for isSorted to use a faster binary search 
+   * ... or, pass a number as the third argument in order to look for the first matching value in the array after the given index
+  */
+  public indexOf<T>(array: Array<T>, value: number, isSorted?: boolean) {
+    if (isSorted == null)
+      return _.indexOf(array, value);
+    return _.indexOf(array, value, isSorted);
+  }
 }
