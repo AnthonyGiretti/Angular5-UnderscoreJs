92f62a0b7d51beb9165b7bf09e10083934c95e27
Anthony Giretti <anthony.giretti@gmail.com>
2018-04-29 18:57:41 -0400

    add zip method declaration

--- Angular5-UnderscoreJs/src/app/services/arrays.ts
+++ Angular5-UnderscoreJs/src/app/services/arrays.ts
@@ -119,4 +119,15 @@ export class ArraysService {
       return _.uniq(array);
     return _.uniq(array, alreadySorted);
   }
+
+  /**
+   * Merges together the values of each of the arrays with the values at the corresponding position. 
+   * Useful when you have separate data sources that are coordinated through matching array indexes. 
+   * Use with apply to pass in an array of arrays. 
+   * If you're working with a matrix of nested arrays, this can be used to transpose the matrix.
+   * @param arraysToZip 
+   */
+  public zip(...arraysToZip: Array<any>): Array<Array<any>> {
+    return _.zip(arraysToZip);
+  }
 }
