343c90a43bcfb1db95b76bc1e6a055bf3438475c
NEXUS\Anthony.Giretti <anthony.giretti@nexusinno.com>
2018-05-02 15:32:10 -0400

    add bind method declaration

--- Angular5-UnderscoreJs/src/app/services/functions.ts
+++ Angular5-UnderscoreJs/src/app/services/functions.ts
@@ -6,5 +6,16 @@ export class FunctionsService {
 
   constructor() { }
 
+  /**
+   * Bind a function to an object, meaning that whenever the function is called, the value of this will be the object. 
+   * Optionally, pass arguments to the function to pre-fill them, also known as partial application. 
+   * For partial application without context binding, use partial.
+   * @param delegate 
+   * @param data 
+   * @param parameters 
+   */
+  public bind<T>(delegate: Function, data: T, ...parameters: Array<any>): Function {
+    return _.bind(delegate, data, ...parameters);
+  }
 
 }
