f40f1653c80acb6fd3b842b9c7c2629f40841ebb
Anthony Giretti <anthony.giretti@gmail.com>
2018-05-06 22:47:00 -0400

    add throtle method declaration

--- Angular5-UnderscoreJs/src/app/services/functions.ts
+++ Angular5-UnderscoreJs/src/app/services/functions.ts
@@ -49,4 +49,21 @@ export class FunctionsService {
   public defer(delegate: Function, ...parameters: Array<any>): void {
     _.defer(delegate, ...parameters);
   }
+
+  /**
+   * Creates and returns a new, throttled version of the passed function,
+   * that, when invoked repeatedly, will only actually call the original function at most once per every wait milliseconds.
+   * Useful for rate-limiting events that occur faster than you can keep up with.
+     By default, throttle will execute the function as soon as you call it for the first time,
+     and, if you call it again any number of times during the wait period, as soon as that period is over.
+     If you'd like to disable the leading-edge call, pass {leading: false},
+     and if you'd like to disable the execution on the trailing-edge, pass
+    {trailing: false}.
+   * @param delegate
+   * @param wait
+   * @param options
+   */
+  public throttle(delegate: Function, wait: number, options: any): Function {
+    return (null == options) ? _.throttle(delegate, wait) : _.throttle(delegate, wait, options);
+  }
 }
