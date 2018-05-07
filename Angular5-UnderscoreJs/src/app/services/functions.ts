00e2923b2a1d63ce73e8ec5211c59b8da13032a8
Anthony Giretti <anthony.giretti@gmail.com>
2018-05-06 22:52:03 -0400

    add debounce function declaration

--- Angular5-UnderscoreJs/src/app/services/functions.ts
+++ Angular5-UnderscoreJs/src/app/services/functions.ts
@@ -66,4 +66,20 @@ export class FunctionsService {
   public throttle(delegate: Function, wait: number, options: any): Function {
     return (null == options) ? _.throttle(delegate, wait) : _.throttle(delegate, wait, options);
   }
+
+  /**
+   * Creates and returns a new debounced version of the passed function
+   * which will postpone its execution until after wait milliseconds have elapsed since the last time it was invoked.
+   * Useful for implementing behavior that should only happen after the input has stopped arriving.
+   * For example: rendering a preview of a Markdown comment, recalculating a layout after the window has stopped being resized,
+   * and so on.
+    At the end of the wait interval,
+    the function will be called with the arguments that were passed most recently to the debounced function.
+    Pass true for the immediate argument to cause debounce to trigger
+    the function on the leading instead of the trailing edge of the wait interval.
+    Useful in circumstances like preventing accidental double-clicks on a "submit" button from firing a second time.
+   */
+  public debounce(delegate: Function, wait: number, immediate?: boolean): Function {
+    return (null == immediate) ? _.debounce(delegate, wait) : _.debounce(delegate, wait, immediate);
+  }
 }
